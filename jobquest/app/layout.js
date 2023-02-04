fc0863f39f94eb90c6ce79737f3dd0ed08e8f959
Andrew Oak <andrewoaksoekhant@gmail.com>
2023-02-04 20:18:26 +0800

    create next 13 app

---
+++ jobquest/app/layout.js
@@ -0,0 +1,16 @@
+import './globals.css'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <nav>Testing</nav>
+        {children}</body>
+    </html>
+  )
+}
