e73095354729db5e589397f129b5cf19b5fdfb77
Andrew Oak <andrewoaksoekhant@gmail.com>
2023-02-05 23:40:34 +0800

    loading page ui

--- jobquest/app/loading.js
+++ jobquest/app/loading.js
@@ -1,3 +1,17 @@
+"use client";
+
+import { Flex, CircularProgress } from "@chakra-ui/react";
+
 export default function Loading() {
-    return <div>Loading...</div>;
+    return (
+        <Flex
+            bg="#EAFDFC"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            height="100%"
+        >
+            <CircularProgress isIndeterminate color="#107CF1" />
+        </Flex>
+    );
 }
