4ac3691fd06f14c2ad54b19ed40362924e91a5fc
Andrew Oak <andrewoaksoekhant@gmail.com>
2023-02-19 23:04:51 +0800

    made anything with "/user" a protected route

--- jobquest/middleware.js
+++ jobquest/middleware.js
@@ -4,11 +4,18 @@ export default function middleware(req) {
     const verify = req.cookies.get("loggedin");
     const url = req.url;
 
-    if (!verify && url.includes("/user/home")) {
-        return NextResponse.redirect("http://localhost:3000/");
+    const openRoutes =
+        url === "http://localhost:3000/" ||
+        url === "http://localhost:3000/login" ||
+        url === "http://localhost:3000/signup";
+
+    const protectedRoutes = url.includes("/user");
+
+    if (verify && openRoutes) {
+        return NextResponse.redirect("http://localhost:3000/user/home");
     }
 
-    if (verify && url === "http://localhost:3000/") {
-        return NextResponse.redirect("http://localhost:3000/user/home");
+    if (!verify && protectedRoutes) {
+        return NextResponse.redirect("http://localhost:3000/");
     }
 }
