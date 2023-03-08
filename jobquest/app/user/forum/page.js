cd661864087fc80ca00abeeede47910884b0b74b
yaomingng <yaomingymym@gmail.com>
2023-03-08 22:32:12 +0800

    Co-authored-by: Andrew Oak <xaynezz@users.noreply.github.com>

--- jobquest/app/user/forum/page.js
+++ jobquest/app/user/forum/page.js
@@ -1,3 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import SearchPost from "./SearchPost";
+import AddPost from "./AddPost";
+import PostList from "./PostList";
+
 export default function ForumPage() {
-    return <div className="h-[calc(100vh-64px)]">Forum</div>;
+    const [addingPost, setAddingPost] = useState(false);
+    const [search, setSearch] = useState("");
+
+    return (
+        <div className="h-[calc(100vh-64px)]">
+            <div className="flex flex-col items-center justify-center gap-2 py-5 text-xs text-black">
+                <header className="flex items-center justify-center gap-1">
+                    <SearchPost search={search} setSearch={setSearch} />
+                    <AddPost
+                        addingPost={addingPost}
+                        setAddingPost={setAddingPost}
+                    />
+                </header>
+                <PostList />
+            </div>
+        </div>
+    );
 }
