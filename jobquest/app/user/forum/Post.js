04e79690859a29665e98d5ad0bc44fca1ef62d7f
yaomingng <yaomingymym@gmail.com>
2023-03-09 23:44:20 +0800

    Co-authored-by: Andrew Oak <xaynezz@users.noreply.github.com>

--- jobquest/app/user/forum/Post.js
+++ jobquest/app/user/forum/Post.js
@@ -1,10 +1,47 @@
+import {
+    ArrowUpIcon,
+    ArrowDownIcon,
+    ChatBubbleBottomCenterIcon,
+} from "@heroicons/react/24/outline";
+
 export default function Post({ postData }) {
     return (
-        <ul className="flex w-[430px] flex-col items-center justify-center rounded-xl bg-accent-100 md:w-[730px]">
-            <li key={postData.postID}>{postData.title}</li>
-            <li>{postData.author}</li>
-            <li>{postData.DatePublished}</li>
-            <li>{postData.content}</li>
+        <ul className="flex w-[430px] flex-col items-start justify-center gap-1 rounded-xl bg-accent-100 px-5 md:w-[730px]">
+            <div className="flex w-full flex-col items-start justify-center pt-3">
+                <button className="text-xl font-bold md:text-2xl">
+                    {postData.title}
+                </button>
+                <div className="flex items-center justify-center gap-2 text-[9px] leading-3 md:text-xs">
+                    <li>{postData.author}</li>
+                    <li>{postData.DatePublished}</li>
+                </div>
+            </div>
+            <li className="max-w-sm overflow-hidden text-ellipsis whitespace-nowrap md:max-w-2xl">
+                {postData.content}
+            </li>
+            <li className="flex w-full items-center justify-between pt-2 pb-6">
+                <div className="flex gap-2">
+                    <div className="flex gap-1">
+                        <button className="h-3 w-3 md:h-4 md:w-4">
+                            <ArrowUpIcon />
+                        </button>
+                        <button className="h-3 w-3 md:h-4 md:w-4">
+                            <ArrowDownIcon />
+                        </button>
+                    </div>
+                    <div className="text-[9px] md:text-xs">
+                        {postData.upvotesNum}
+                    </div>
+                </div>
+                <div className="flex items-center justify-center gap-2">
+                    <div className="h-3 w-3 md:h-5 md:w-5">
+                        <ChatBubbleBottomCenterIcon />
+                    </div>
+                    <div className="text-[9px] md:text-xs">
+                        {postData.commentNum}
+                    </div>
+                </div>
+            </li>
         </ul>
     );
 }
