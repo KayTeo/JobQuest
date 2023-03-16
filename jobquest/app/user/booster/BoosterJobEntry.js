738ae16b6067e918e31195882f6b75f2e5d7c8e1
yaomingng <yaomingymym@gmail.com>
2023-03-16 19:40:36 +0800

    done booster page

---
+++ jobquest/app/user/booster/BoosterJobEntry.js
@@ -0,0 +1,149 @@
+"use client";
+
+import { useState } from "react";
+
+export default function BoosterJobEntry({ data }) {
+    const [letterFlag, setLetterFlag] = useState(false);
+    const [resumeFlag, setResumeFlag] = useState(false);
+    const [resume, setResume] = useState(null);
+    const [letter, setLetter] = useState(null);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        let resumeFlag = false;
+        try {
+            console.log(e.target.resume.value);
+            resumeFlag = true;
+        } catch (e) {}
+        try {
+            console.log(e.target.letter.value);
+        } catch (e) {}
+
+        //boost resume/letter here
+
+        if (resumeFlag) {
+            setResume(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            );
+            setLetter(null);
+        } else {
+            setLetter(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            );
+            setResume(null);
+        }
+    }
+
+    return (
+        <div className="flex w-[350px] flex-col items-center justify-center gap-1 rounded-3xl border border-black bg-light-200 pt-2 pb-5 text-black md:w-[550px]">
+            <section className="flex w-[320px] items-center justify-center overflow-hidden text-ellipsis whitespace-nowrap font-bold md:w-[520px]">
+                {data.jobTitle}
+            </section>
+            <section className="flex w-[320px] items-center justify-center overflow-hidden text-ellipsis whitespace-nowrap md:w-[520px]">
+                {data.company.name}
+            </section>
+            <section className="flex gap-2 px-5">
+                <button
+                    onClick={() => {
+                        setResumeFlag(!resumeFlag);
+                        setLetterFlag(false);
+                    }}
+                    className="h-6 w-32 rounded-full bg-accent-500 text-center text-xs font-bold leading-6 text-white shadow-sm hover:bg-accent-300"
+                >
+                    Boost Resume
+                </button>
+                <button
+                    onClick={() => {
+                        setLetterFlag(!letterFlag);
+                        setResumeFlag(false);
+                    }}
+                    className="h-6 w-32 rounded-full bg-accent-500 text-center text-xs font-bold leading-6 text-white shadow-sm hover:bg-accent-300"
+                >
+                    Boost Cover Letter
+                </button>
+            </section>
+            {resumeFlag && (
+                <form
+                    id="resumeForm"
+                    onSubmit={handleSubmit}
+                    action="submit"
+                    className="flex flex-col gap-2"
+                >
+                    <div className="flex w-64 flex-col gap-1 md:w-80">
+                        <label htmlFor="resume" className="font-bold">
+                            Resume:
+                        </label>
+                        <textarea
+                            id="resume"
+                            rows="8"
+                            placeholder="Insert Your Resume..."
+                            className="rounded-xl border border-black p-1 text-xs"
+                        ></textarea>
+                    </div>
+                    <div className="flex w-full justify-between">
+                        <div></div>
+                        <button
+                            type="submit"
+                            form="resumeForm"
+                            className="h-7 w-16 rounded-full bg-accent-500 text-center text-xs font-bold leading-6 text-white shadow-sm hover:bg-accent-300 md:h-8 md:w-20 md:text-sm"
+                        >
+                            Boost
+                        </button>
+                    </div>
+                </form>
+            )}
+            {letterFlag && (
+                <form
+                    id="letterForm"
+                    onSubmit={handleSubmit}
+                    action="submit"
+                    className="flex flex-col gap-2"
+                >
+                    <div className="flex w-64 flex-col gap-1 md:w-80">
+                        <label htmlFor="letter" className="font-bold">
+                            Cover Letter:
+                        </label>
+                        <textarea
+                            id="letter"
+                            rows="8"
+                            placeholder="Insert Your Cover Letter..."
+                            className="rounded-xl border border-black p-1 text-xs"
+                        ></textarea>
+                    </div>
+                    <div className="flex w-full justify-between">
+                        <div></div>
+                        <button
+                            type="submit"
+                            form="letterForm"
+                            className="h-7 w-16 rounded-full bg-accent-500 text-center text-xs font-bold leading-6 text-white shadow-sm hover:bg-accent-300 md:h-8 md:w-20 md:text-sm"
+                        >
+                            Boost
+                        </button>
+                    </div>
+                </form>
+            )}
+            {(resume || letter) && (
+                <div className="flex flex-col gap-5 pb-8 pt-3">
+                    {resume && (
+                        <div className="flex w-64 flex-col gap-1 md:w-80">
+                            <div className="font-bold">Boosted Resume:</div>
+                            <div className="rounded-xl border border-black bg-white p-1 text-xs">
+                                {resume}
+                            </div>
+                        </div>
+                    )}
+                    {letter && (
+                        <div className="flex w-64 flex-col gap-1 md:w-80">
+                            <div className="font-bold">
+                                Boosted Cover Letter:
+                            </div>
+                            <div className="rounded-xl border border-black bg-white p-1 text-xs">
+                                {letter}
+                            </div>
+                        </div>
+                    )}
+                </div>
+            )}
+        </div>
+    );
+}
--- jobquest/app/user/booster/page.js
+++ jobquest/app/user/booster/page.js
@@ -1,3 +1,23 @@
+import { track } from "../tracker/tempdata";
+import BoosterJobEntry from "./boosterJobEntry";
+
 export default function BoosterPage() {
-    return <div className="h-[calc(100vh-64px)]">Booster</div>;
+    const trackData = track;
+
+    return (
+        <div className="h-[calc(100vh-64px)] overflow-auto">
+            <div className="flex flex-col items-center justify-center gap-2 py-10">
+                <header className="text-2xl font-bold text-accent-500 md:text-3xl">
+                    Choose Job Target
+                </header>
+                <main className="text-black">
+                    <section className="flex flex-col items-center justify-center gap-2">
+                        {trackData.map((e) => (
+                            <BoosterJobEntry key={e.uuid} data={e} />
+                        ))}
+                    </section>
+                </main>
+            </div>
+        </div>
+    );
 }
